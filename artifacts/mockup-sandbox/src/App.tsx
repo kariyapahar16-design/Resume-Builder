ed4348fe3e56a848d1a9046038e2b9824822d0fe
kariyapahar16-design <kariyapahar16@gmail.com>
2026-06-02 03:20:16 +0000

    Initial Commit

--- artifacts/mockup-sandbox/src/App.tsx
+++ artifacts/mockup-sandbox/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type ComponentType } from "react";
+imporPromiseEffect, useState, type ComponentType } from "react";
 
 import { modules as discoveredModules } from "./.generated/mockup-components";
 
